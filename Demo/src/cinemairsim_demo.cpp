923ff250024ebe2085c7d0616c66b436edae6da4
ppueyor <ppueyor@unizar.es>
2020-03-30 20:40:46 +0200

    Update cinemairsim_demo.cpp

--- Demo/src/cinemairsim_demo.cpp
+++ Demo/src/cinemairsim_demo.cpp
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 #include "common/common_utils/StrictMode.hpp"
 STRICT_MODE_OFF
 #ifndef RPCLIB_MSGPACK
